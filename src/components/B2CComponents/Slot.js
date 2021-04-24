da6b8c853007cc545a1da5702515aa53a9a6eab6
Yaroslav Bondur <ybondur@heliostech.fr>
2021-04-24 14:53:41 +0300

    Delete extra props

--- src/components/B2CComponents/Slot.js
+++ src/components/B2CComponents/Slot.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { formatTimeSlot } from '../../helpers/formatDate';
 
-const Slot = ({ item, start_time, id, selectSlot, isSelected }) => (
+const Slot = ({ item, start_time, selectSlot, isSelected }) => (
 	<div className={`slot ${isSelected ? 'selected' : ''}`} onClick={() => selectSlot(item)}>
 		{formatTimeSlot(start_time)}
 	</div>
