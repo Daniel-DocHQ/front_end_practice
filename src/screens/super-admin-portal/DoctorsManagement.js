88a1ce95638e6961ef5d95d9c04997c6a5bb215e
Peter Holt <peter.holt@dochq.co.uk>
2021-05-11 12:18:18 +0100

    Added

--- src/screens/super-admin-portal/DoctorsManagement.js
+++ src/screens/super-admin-portal/DoctorsManagement.js
@@ -15,8 +15,8 @@ const DoctorsManagement = props => {
 	const [isLoading, setIsLoading] = useState(true);
 	const today = new Date();
 	today.setHours(0,0,0,0);
-	const start_time = new Date(new Date().setDate(today.getDate() - 180));
-	const end_time = new Date(today.setDate(today.getDate() + 180));
+	const start_time = new Date(new Date().setDate(today.getDate() - 50));
+	const end_time = new Date(today.setDate(today.getDate() + 120));
 	let history = useHistory();
 
 	const logoutUser = () => {
