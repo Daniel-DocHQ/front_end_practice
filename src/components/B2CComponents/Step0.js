c83f8a45e33a62e79f03c6cedf0569570dda8df3
Yaroslav Bondur <ybondur21@gmail.com>
2021-06-18 15:22:52 +0300

    Rename product in booking form (#224)
    Co-authored-by: Yaroslav Bondur <ybondur@heliostech.fr>

--- src/components/B2CComponents/Step0.js
+++ src/components/B2CComponents/Step0.js
@@ -16,7 +16,7 @@ const ADDITIONAL_PRODUCT_TEXT = {
     'Fit to Travel [PCR]': 'appointment available between 72 hours and 57 hours prior to your flight',
     'Day 2 PCR Test': 'book an appointment within 2 days of your arrival in the UK',
     'Day 8 PCR Test': 'book an appointment on the 8th day after your arrival in the UK',
-    'Pre Departure Test [United Kingdom]': 'book an appointment with 48 hours of your travel',
+    'Pre Departure Antigen Test [to England]': 'book an appointment with 48 hours of your travel',
     'Amber Short Stay': 'book an appointment within 2 days of your arrival in the UK',
     'Test to Release': 'book an appointment on the 5th day after your arrival in the UK',
 };
