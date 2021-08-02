b3a0a3ef51b817a5e99eede40520bf5f7fa84673
Peter Holt <peter.holt@dochq.co.uk>
2021-08-02 11:30:20 +0100

    Updated URL

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -9,7 +9,7 @@ const adminService = {
 			if (typeof token !== 'undefined') {
 				axios({
 					method: 'get',
-					url: `${baseUrl}/admin/appointments`,
+					url: `${baseUrl}/admin/allappointments`,
 					headers: { Authorization: `Bearer ${token}` },
 				})
 					.then(response => {
