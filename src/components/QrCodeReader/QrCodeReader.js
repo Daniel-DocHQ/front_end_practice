1b95fedf6bae5f86c1700be19d14d5aa44281151
Yaroslav Bondur <ybondur21@gmail.com>
2021-10-20 10:56:10 +0300

    Update QR code reader (#439)
    Co-authored-by: Yaroslav Bondur <ybondur@heliostech.fr>

--- src/components/QrCodeReader/QrCodeReader.js
+++ src/components/QrCodeReader/QrCodeReader.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import QrReader from 'react-qr-scanner'
 import DocButton from '../DocButton/DocButton';
 
 const QrCodeReader = ({ handleResult }) => {
   const [result, setResult] = useState();
   const [scan, setScan] = useState(false);
+  const qrCodeReader = useRef(null);
 
   const previewStyle = {
     height: 240,
@@ -14,21 +15,30 @@ const QrCodeReader = ({ handleResult }) => {
   return (
     <div>
       {scan ? (
-        <QrReader
-          delay={100}
-          style={previewStyle}
-          facingMode="rear"
-          legacyMode
-          onError={(err) => console.error(err)}
-          onScan={(data) => {
-            if (!!data) {
-              const { text } = data;
-              setResult(text);
-              handleResult(text);
-              setScan(false);
-            }
-          }}
-        />
+        <>
+          <QrReader
+            ref={qrCodeReader}
+            delay={100}
+            style={previewStyle}
+            facingMode="rear"
+            chooseDeviceId={1}
+            legacyMode
+            onError={(err) => console.error(err)}
+            onScan={(data) => {
+              if (!!data) {
+                const { text } = data;
+                setResult(text);
+                handleResult(text);
+                setScan(false);
+              }
+            }}
+          />
+          {(!!qrCodeReader && !!qrCodeReader.current) && (
+            <div className="row">
+              <DocButton color="green" onClick={() => qrCodeReader.current.openImageDialog()} text="Submit a QR Code"/>
+            </div>
+          )}
+        </>
       ) : (
         <DocButton text="Scan QR" onClick={() => setScan(true)} color="green"/>
       )}
