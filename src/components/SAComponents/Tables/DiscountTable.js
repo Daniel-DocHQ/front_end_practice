9967d17e754073ca18592a801379734b702ec79c
Yaroslav Bondur <ybondur21@gmail.com>
2022-01-14 16:51:32 +0200

    Add collapse to Discount code tables (#544)
    Co-authored-by: Yaroslav Bondur <ybondur@heliostech.fr>

--- src/components/SAComponents/Tables/DiscountTable.js
+++ src/components/SAComponents/Tables/DiscountTable.js
@@ -1,6 +1,9 @@
-import React  from 'react';
+import React, { useState } from 'react';
 import { get } from 'lodash';
 import { format } from 'date-fns';
+import Collapse from '@material-ui/core/Collapse';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -9,6 +12,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import LinkButton from '../../DocButton/LinkButton';
 import '../../Tables/Tables.scss';
+import LoadingSpinner from '../../LoadingSpinner/LoadingSpinner';
 
 const styles = {
 	tableText: {
@@ -26,120 +30,141 @@ const styles = {
 		display: 'flex',
 		justifyContent: 'space-between',
 		alignItems: 'center',
+        cursor: 'pointer',
     },
     snackbar: {
         color: '#FFF',
     }
 };
 
-const DiscountTable = ({ isUsed = false, discounts = [] }) => {
+const DiscountTable = ({ isUsed = false, discounts = [], loading }) => {
+	const [checked, setChecked] = useState(false);
     const sortedDiscounts = discounts.sort(({ created_at: aStartTime }, { created_at: bStartTime }) => new Date(bStartTime * 1000).getTime() - new Date(aStartTime * 1000).getTime());
+
+    const handleChange = () => {
+		setChecked((prev) => !prev);
+	};
 
     return (
        <div className='doc-container tables' style={{ justifyContent: 'unset' }}>
-            <div style={styles.mainContainer}>
+            <div style={styles.mainContainer} onClick={handleChange}>
                 <h2>{isUsed && 'Used '}Discount List</h2>
+                {checked ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
             </div>
-            <TableContainer
-                style={{
-                    marginBottom: '40px',
-                }}
-            >
-                <Table stickyHeader>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align='left' style={styles.tableText}>Project</TableCell>
-                            <TableCell align='center' style={styles.tableText}>Code</TableCell>
-                            <TableCell align='center' style={styles.tableText}>Uses</TableCell>
-                            <TableCell align='center' style={styles.tableText}>Value</TableCell>
-                            <TableCell align='center' style={styles.tableText}>Generated</TableCell>
-                            <TableCell align='center' style={styles.tableText}>Valid From</TableCell>
-                            <TableCell align='center' style={styles.tableText}>Valid Until</TableCell>
-                            {isUsed && (
-                                <TableCell align='right' style={styles.tableText}>Actions</TableCell>
-                            )}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {typeof sortedDiscounts !== 'undefined' &&
-                            typeof sortedDiscounts === 'object' &&
-                            sortedDiscounts.length > 0 &&
-                            sortedDiscounts.map((discount, indx) => (
-                                <TableRow key={indx}>
-                                    <TableCell
-                                        align='left'
-                                        style={{ ...styles.tableText }}
-                                    >
-                                        DocHQ
-                                    </TableCell>
-                                    <TableCell
-                                        align='center'
-                                        style={{ ...styles.tableText }}
-                                    >
-                                        {get(discount, 'code', '')}
-                                    </TableCell>
-                                    <TableCell
-                                        align='center'
-                                        style={{ ...styles.tableText }}
-                                    >
-                                        {get(discount, 'uses', 0)}
-                                    </TableCell>
-                                    <TableCell
-                                        align='center'
-                                        style={{ ...styles.tableText }}
-                                    >
-                                        {get(discount, 'value', '')}{get(discount, 'type', '') === 'percentage' ? '%' : '£'}
-                                    </TableCell>
-                                    <TableCell
-                                        align='center'
-                                        style={{ ...styles.tableText }}
-                                    >
-                                        {format(new Date(get(discount, 'created_at', new Date().getTime()) * 1000), 'dd/MM/yyyy p')}
-                                    </TableCell>
-                                    <TableCell
-                                        align='center'
-                                        style={{ ...styles.tableText }}
-                                    >
-                                        {format(new Date(get(discount, 'active_from', new Date())), 'dd/MM/yyyy')}
-                                    </TableCell>
-                                    <TableCell
-                                        align='center'
-                                        style={{ ...styles.tableText }}
-                                    >
-                                        {format(new Date(get(discount, 'active_to', new Date())), 'dd/MM/yyyy')}
-                                    </TableCell>
-                                    {isUsed && (
-                                        <TableCell align='right' style={{ ...styles.tableText }}>
-                                            <div style={{ display: 'inline-flex' }}>
-                                                <div style={{ margin: '0 10px' }}>
-                                                    <LinkButton
-                                                        text="View Order"
-                                                        color='green'
-                                                        linkSrc={`/super_admin/order-list?discount=${discount.code}`}
-                                                    />
-                                                </div>
-                                            </div>
-                                        </TableCell>
-                                    )}
-                                </TableRow>
-                            ))}
-                        {typeof sortedDiscounts !== 'object' || sortedDiscounts.length === 0 ? (
+            <Collapse in={checked}>
+                <TableContainer
+                    style={{
+                        marginBottom: '40px',
+                    }}
+                >
+                    <Table stickyHeader>
+                        <TableHead>
                             <TableRow>
-                                <TableCell style={styles.tableText}>
-                                    <p>No discounts to display</p>
-                                </TableCell>
-                                <TableCell />
-                                <TableCell />
-                                <TableCell />
-                                <TableCell />
-                                <TableCell />
-                                <TableCell />
-                                <TableCell />
+                                <TableCell align='left' style={styles.tableText}>Project</TableCell>
+                                <TableCell align='center' style={styles.tableText}>Code</TableCell>
+                                <TableCell align='center' style={styles.tableText}>Uses</TableCell>
+                                <TableCell align='center' style={styles.tableText}>Value</TableCell>
+                                <TableCell align='center' style={styles.tableText}>Generated</TableCell>
+                                <TableCell align='center' style={styles.tableText}>Valid From</TableCell>
+                                <TableCell align='center' style={styles.tableText}>Valid Until</TableCell>
+                                {isUsed && (
+                                    <TableCell align='right' style={styles.tableText}>Actions</TableCell>
+                                )}
                             </TableRow>
-                        ) : null}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {loading ? (
+                                <>
+                                    <TableCell />
+                                    <TableCell />
+                                    <TableCell />
+                                    <TableCell />
+                                    <TableCell>
+                                        <LoadingSpinner />
+                                    </TableCell>
+                                    <TableCell />
+                                    <TableCell />
+                                    {isUsed && (
+                                        <TableCell />
+                                    )}
+                                </>
+                            ) : sortedDiscounts.length > 0 ? (
+                                sortedDiscounts.map((discount, indx) => (
+                                    <TableRow key={indx}>
+                                        <TableCell
+                                            align='left'
+                                            style={{ ...styles.tableText }}
+                                        >
+                                            DocHQ
+                                        </TableCell>
+                                        <TableCell
+                                            align='center'
+                                            style={{ ...styles.tableText }}
+                                        >
+                                            {get(discount, 'code', '')}
+                                        </TableCell>
+                                        <TableCell
+                                            align='center'
+                                            style={{ ...styles.tableText }}
+                                        >
+                                            {get(discount, 'uses', 0)}
+                                        </TableCell>
+                                        <TableCell
+                                            align='center'
+                                            style={{ ...styles.tableText }}
+                                        >
+                                            {get(discount, 'value', '')}{get(discount, 'type', '') === 'percentage' ? '%' : '£'}
+                                        </TableCell>
+                                        <TableCell
+                                            align='center'
+                                            style={{ ...styles.tableText }}
+                                        >
+                                            {format(new Date(get(discount, 'created_at', new Date().getTime()) * 1000), 'dd/MM/yyyy p')}
+                                        </TableCell>
+                                        <TableCell
+                                            align='center'
+                                            style={{ ...styles.tableText }}
+                                        >
+                                            {format(new Date(get(discount, 'active_from', new Date())), 'dd/MM/yyyy')}
+                                        </TableCell>
+                                        <TableCell
+                                            align='center'
+                                            style={{ ...styles.tableText }}
+                                        >
+                                            {format(new Date(get(discount, 'active_to', new Date())), 'dd/MM/yyyy')}
+                                        </TableCell>
+                                        {isUsed && (
+                                            <TableCell align='right' style={{ ...styles.tableText }}>
+                                                <div style={{ display: 'inline-flex' }}>
+                                                    <div style={{ margin: '0 10px' }}>
+                                                        <LinkButton
+                                                            text="View Order"
+                                                            color='green'
+                                                            linkSrc={`/super_admin/order-list?discount=${discount.code}`}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </TableCell>
+                                        )}
+                                    </TableRow>
+                                ))) : (
+                                <TableRow>
+                                    <TableCell style={styles.tableText}>
+                                        <p>No discounts to display</p>
+                                    </TableCell>
+                                    <TableCell />
+                                    <TableCell />
+                                    <TableCell />
+                                    <TableCell />
+                                    <TableCell />
+                                    <TableCell />
+                                    <TableCell />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Collapse>
         </div>
     );
 };
--- src/screens/super-admin-portal/SADiscountManagement.js
+++ src/screens/super-admin-portal/SADiscountManagement.js
@@ -12,12 +12,14 @@ import { DISCOUNT_USER_NAMES } from '../../helpers/permissions';
 const SADiscountManagement = ({ token, role, isAuthenticated, user }) => {
     const userName = `${get(user, 'first_name', '')} ${get(user, 'last_name', '')}`;
 	const { logout } = useContext(AuthContext);
+	const [loading, setLoading] = useState(true);
 	const [discounts, setDiscounts] = useState();
     const [usedDiscounts, setUsedDiscounts] = useState();
 	let history = useHistory();
 
-	const getDiscounts = async () => (
-		adminService
+	const getDiscounts = async () => {
+		setLoading(true);
+		await adminService
 			.getDiscounts(token)
 			.then(data => {
 				if (data.success) {
@@ -29,8 +31,9 @@ const SADiscountManagement = ({ token, role, isAuthenticated, user }) => {
 					ToastsStore.error('Error fetching Discounts');
 				}
 			})
-			.catch(err => ToastsStore.error('Error fetching Discounts'))
-    );
+			.catch(err => ToastsStore.error('Error fetching Discounts'));
+		setLoading(false);
+	};
 	const logoutUser = () => {
 		logout();
 		history.push('/login');
@@ -47,10 +50,19 @@ const SADiscountManagement = ({ token, role, isAuthenticated, user }) => {
         <DiscountAppBar value={1}>
             <Grid container justify="space-between">
                 <Grid item xs={12}>
-                    <DiscountTable token={token} reload={getDiscounts} discounts={discounts} />
+                    <DiscountTable
+						token={token}
+						loading={loading}
+						reload={getDiscounts}
+						discounts={discounts}
+					/>
                 </Grid>
                 <Grid item xs={12}>
-                    <DiscountTable isUsed discounts={usedDiscounts} />
+                    <DiscountTable
+						isUsed
+						loading={loading}
+						discounts={usedDiscounts}
+					/>
                 </Grid>
             </Grid>
         </DiscountAppBar>
