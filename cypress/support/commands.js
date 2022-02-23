21d7853b85443d9e80490f7b24a8ad06916a4f28
Daniel-DocHQ <daniel.albul@dochq.co.uk>
2022-02-23 11:54:59 +0200

    Refactor iframe handling for Stripe

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,28 +2,47 @@ import 'cypress-fill-command'
 import accounts from '../fixtures/accounts'
 
 Cypress.Commands.add('getIframe', (iframe) => {
+
     return cy.get(iframe)
-        .its('0.contentDocument.body')
-        .should('be.visible')
-        .then(cy.wrap);
+        .its('0.contentDocument.body').should('not.be.empty')
+        .then(cy.wrap)
 })
 
-Cypress.Commands.add('login', (user) => {		
-    
+
+Cypress.Commands.add('iframeLoaded', { prevSubject: 'element' }, $iframe => {
+    const contentWindow = $iframe.prop('contentWindow')
+    return new Promise(resolve => {
+        if (contentWindow && contentWindow.document.readyState === 'complete') {
+            resolve(contentWindow)
+        } else {
+            $iframe.on('load', () => {
+                resolve(contentWindow)
+            })
+        }
+    })
+})
+
+Cypress.Commands.add('getInDocument', { prevSubject: 'document' }, (document, selector) =>
+    Cypress.$(selector, document),
+)
+
+
+Cypress.Commands.add('login', (user) => {
+
     let email = accounts[user].email;
     let password = accounts[user].password;
 
-	cy.visit('https://myhealth-staging.dochq.co.uk/login')
-	cy.intercept({method: 'GET', url: 'https://ui-identity-editor-staging.dochq.co.uk/login?client=dochqhealth',}).as('login_page')
-	cy.get('.login-container').find('button.green').click()
-	cy.wait('@login_page')
-	cy.wait(1000)
+    cy.visit('https://myhealth-staging.dochq.co.uk/login')
+    cy.intercept({ method: 'GET', url: 'https://ui-identity-editor-staging.dochq.co.uk/login?client=dochqhealth', }).as('login_page')
+    cy.get('.login-container').find('button.green').click()
+    cy.wait('@login_page')
+    cy.wait(1000)
 
-	cy.get('input[type="email"]').type(email)
-	cy.get('input[type="password"]').type(password)
+    cy.get('input[type="email"]').type(email)
+    cy.get('input[type="password"]').type(password)
 
-	cy.get('button').contains('Login').click({force:true})
-	cy.wait(2000)
+    cy.get('button').contains('Login').click({ force: true })
+    cy.wait(2000)
 })
 
 Cypress.Commands.add('logout', () => {
