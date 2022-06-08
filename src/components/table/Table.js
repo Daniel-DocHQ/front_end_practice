3858865d3b0bf6e769bfe63926786236c6b128d1
DanAlbul <daniel.albul@dochq.co.uk>
2022-06-08 12:14:41 +0300

    Refactoring CReateTable function

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -7,18 +7,35 @@ export class Table extends ExcelComponent {
   constructor($root) {
     super($root, {
       name: "Table",
-      listeners: ["input"],
+      listeners: ["click", "mousedown", "mouseup", "mousemove"],
     });
   }
+  //
 
   toHTML() {
-    const rows = createTable(100);
+    return createTable(100);
     // Appending each row from list of rows to excel__table <div>
-    return rows.forEach((row) => this.$root.append(row));
+    //return rows.forEach((row) => this.$root.append(row));
   }
 
   onInput(event) {
     console.log(this.$root); //
     console.log(`${this.className} onInput`, event);
+  }
+
+  onClick(event) {
+    console.log(`click`, event);
+  }
+
+  onMousedown(event) {
+    console.log(`mousedown`, event);
+  }
+
+  onMouseup(event) {
+    console.log(`mousup`, event);
+  }
+
+  onMousemove(event) {
+    console.log(`mousemove`);
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,43 +1,46 @@
-import { $ } from "../../core/dom";
-
 const CODES = {
-  A: "65",
-  Z: "90",
+  A: 65,
+  Z: 90,
 };
 
-export function createTable(rowsCount = 50) {
+function toCell() {
+  return `
+    <div class="cell" contenteditable></div>
+  `;
+}
+
+function toColumn(col) {
+  return `
+    <div class="column">${col}</div>
+  `;
+}
+
+function createRow(index, content) {
+  return `
+    <div class="row">
+      <div class="row-info">${index ? index : ""}</div>
+      <div class="row-data">${content}</div>
+    </div>
+  `;
+}
+
+function toChar(_, index) {
+  return String.fromCharCode(CODES.A + index);
+}
+
+export function createTable(rowsCount = 15) {
+  const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
-  const colsCount = CODES.Z - CODES.A;
-  const colName = (num) => `${String.fromCharCode(num)}`;
 
-  for (let rowIndex = 0; rowIndex <= rowsCount; rowIndex++) {
-    const row = $.create("div", "row");
+  const cols = new Array(colsCount).fill("").map(toChar).map(toColumn).join("");
 
-    /*  Row Data consists of namedCells (A, B, C, D, E...) 
-		and working cells (empty cells)   */
-    const rowData = $.create("div", "row-data");
+  rows.push(createRow(null, cols));
 
-    /*  Creating first infoCell and numeric cells 
-		for each subsequent row 1, 2, 3, 4...  */
-    const infoCell = $.create("div", "row-info").html("");
-    const rowInfo = $.create("div", "row-info").html(`${rowIndex}`);
-    if (rowIndex == 0) row.append(infoCell);
-    else row.append(rowInfo);
+  for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount).fill("").map(toCell).join("");
 
-    /*  Creating namedCells (columns) in the first row 
-        and working cells for each subsequent row   */
-    for (let colIndex = 0; colIndex <= colsCount; colIndex++) {
-      const infoColumn = $.create("div", "column").html(colName(CODES.A++));
-      const col = $.create("div", "cell").attribute("contenteditable");
-      if (rowIndex == 0) rowData.append(infoColumn);
-      else rowData.append(col);
-    }
-
-    // Appending final rows to the table
-    row.append(rowData);
-    // Each row is separate block
-    rows.push(row);
+    rows.push(createRow(i + 1, cells));
   }
 
-  return rows;
+  return rows.join("");
 }
